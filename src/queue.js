8fa95e661f1ac9c800118ac41d1319ff642430e5
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-01 17:26:43 +0300

    feat: make Queue and Stack

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  queue = null;
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+    let currentNode = this.queue;
+
+    if(!currentNode) {
+      this.queue = node;      
+      return node;
+    }
+
+    while(currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = node;
+    return node;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.queue.value;
+    this.queue = this.queue.next;
+    return result;
   }
 }
 
