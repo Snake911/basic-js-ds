44d60ddcaef8eec19623f31011b17d533449e58e
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-02 11:19:48 +0300

    feat: make Remove from list and Binary search tree (not remove)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,94 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor(data) {
+    this.tree = data ? new Node(data) : null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const node = new Node(data);
+    let current = this.tree;
+    if(!current) {
+      this.tree = node;
+      return;
+    }
+    while(data !== current.data) {
+      if(data > current.data) {
+        if(current.right) {
+          current = current.right;
+        } else {
+          current.right = node;
+          return;
+        }
+      }
+      if(data < current.data) {
+        if(current.left) {
+          current = current.left;
+        } else {
+          current.left = node;
+          return;
+        }
+      }
+    }
+    return;
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return !!this.find(data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+   let current = this.tree;
+   while(data !== current.data) {
+    if(data > current.data) {
+      if(current.right) {
+        current = current.right;
+      } else {
+        return null;
+      }
+    }
+    if(data < current.data) {
+      if(current.left) {
+        current = current.left;
+      } else {
+        return null;
+      }
+    }
+   }
+   return current;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    let current = this.find(data);
+    if(!current) {
+      return;
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.tree;
+
+    if(!current) return null;
+    while(current.left) {
+      current = current.left;
+    }
+    return current.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.tree;
+
+    if(!current) return null;
+    while(current.right) {
+      current = current.right;
+    }
+    return current.data;
   }
 }
 
